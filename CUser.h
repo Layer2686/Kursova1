b65280cb9b636c33e5bd85a46d4fc71f6a79569f
Layer2686 <baster2686@gmail.com>
2025-04-27 18:47:54 +0300

    Initial commit

---
+++ CUser.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <string>
+class CUser {
+private:
+    std::string username;
+    std::string password;
+
+public:
+    CUser(const std::string& name, const std::string& pass);
+    virtual ~CUser() = default;
+
+    bool login(const std::string& name, const std::string& pass) const;
+    std::string getUsername() const;
+
+    static bool loginFromFile(const std::string& username, const std::string& password);
+    static bool registerUser(const std::string& username, const std::string& password);
+    void switchUser();
+};
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "CUser.h"
+
+int main()
+{
+    CUser user("", ""); // Початкові дані пусті, бо вводяться після вибору опції
+    user.switchUser();
+    return 0;
+}
